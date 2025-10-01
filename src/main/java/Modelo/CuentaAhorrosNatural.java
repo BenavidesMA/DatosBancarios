794b509e0563f7477ed0c1a416bdc80d6626f0e9
Miguel <miguelbv2302@gmail.com>
2025-10-01 11:26:08 -0500

    Se creó la clase CuentaAhorrosNatural

---
+++ src/main/java/Modelo/CuentaAhorrosNatural.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Miguel
+ */
+public class CuentaAhorrosNatural extends CuentaAhorros {
+    
+    public CuentaAhorrosNatural(String nombre, String codigo, int saldo) {
+        super(nombre, codigo, saldo);
+    }
+    
+}
