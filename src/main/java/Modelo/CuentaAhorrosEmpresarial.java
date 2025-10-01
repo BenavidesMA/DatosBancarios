cda72b90d952de8948cf00a543df75afb6bc65e8
Miguel <miguelbv2302@gmail.com>
2025-10-01 11:28:10 -0500

    Se creó la clase CuentaAhorrosEmpresarial

---
+++ src/main/java/Modelo/CuentaAhorrosEmpresarial.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Miguel
+ */
+public class CuentaAhorrosEmpresarial extends CuentaAhorros{
+    
+    public CuentaAhorrosEmpresarial(String nombre, String codigo, int saldo) {
+        super(nombre, codigo, saldo);
+    }
+    
+}
